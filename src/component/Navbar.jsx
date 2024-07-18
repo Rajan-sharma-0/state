2e89aa893646413adf5407476147e230cfcb5347
Rajan-sharma-0 <nk800109@gmail.com>
2024-07-18 16:26:35 +0530

    changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import logo from '../images/logo1.png';
-
+import menu from '../images/menu.png'
+import close from '../images/close.png'
 
 function Navbar() {
+  const handleClick= ()=>{
+    document.querySelector('.menu').style.display='flex'
+    document.querySelector('.close').style.display='block'
+    document.querySelector('.menus').style.display='none'
+  }
+  const handleClose = ()=>{
+ document.querySelector('.menu').style.display='none'
+    document.querySelector('.close').style.display='none'
+    document.querySelector('.menus').style.display='block'
+
+  }
   return (
     <nav>
         <a href="#" className='logo'>
-          <img src={logo} alt="logo" />
-        </a>
+          <img src={logo} alt="logo"  className='logoimage'/>
+          </a>
+          <a href="#" id='openandclose'>
+          <img src={menu} alt="menu" id='open'  className='hamburger menus' onClick={handleClick}/>
+          <img src={close} alt="menu" id='close'  className='hamburger close' onClick={handleClose}/>
+          </a>
         
         {/* <input className='menu-btn' type="checkbox" id='menu-btn' /> */}
         {/* <label className="menu-icon" >
